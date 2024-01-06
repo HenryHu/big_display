0c86fbead1b61c7c0ebc77eb132e1a1a9b2ab552
Henry Hu <henry.hu.sh@gmail.com>
2024-01-06 12:43:18 -0500

    memory leak fix: do not send multiple requests

--- src/cmds.cpp
+++ src/cmds.cpp
@@ -96,7 +96,21 @@ void HandleDot(AsyncWebServerRequest *request) {
 
 void HandleBitmap(AsyncWebServerRequest *request, uint8_t *data, size_t len,
         size_t index, size_t total) {
-    Serial.printf("Body handler: %d %d %d\n\r", len, index, total);
+    Serial.printf("Bitmap body handler: %d %d %d\n\r", len, index, total);
+
+    if (request->_tempObject == nullptr) {
+        request->_tempObject = malloc(total);
+    }
+
+    unsigned char *buf = (unsigned char*)request->_tempObject;
+    for (int i = 0; i < len; ++i) {
+        buf[i + index] = data[i];
+    }
+}
+
+void HandleBitmapEnd(AsyncWebServerRequest * request){
+    Serial.printf("Bitmap request end handler\n\r");
+
     int x = 0, y = 0, w = 0, h = 0;
     if (request->hasParam("x")) {
         x = request->getParam("x")->value().toInt();
@@ -115,48 +129,42 @@ void HandleBitmap(AsyncWebServerRequest *request, uint8_t *data, size_t len,
         w = display->height();
     }
     if (x < 0 || y < 0 || w < 0 || h < 0 || x >= display->width() || y >= display->height()) {
-        if (index == 0) {
-            request->send(400, "text/plain", "input args out of range");
-        }
+        request->send(400, "text/plain", "input args out of range");
         return;
     }
 
-    if (total < w * h * 2) {
-        if (index == 0) {
-            request->send(400, "text/plain", String("body too short: ") + String(len));
-        }
+    const int len = request->contentLength();
+    if (len < w * h * 2) {
+        request->send(400, "text/plain", String("body too short: ") + String(len));
         return;
     }
 
-    if (request->_tempObject == nullptr) {
-        request->_tempObject = malloc(total);
+    unsigned char* buf = (unsigned char*)request->_tempObject;
+    if (buf == nullptr) {
+        request->send(500, "text/plain", "insufficient data");
+        return;
     }
 
-    char* buf = (char*)request->_tempObject;
-    for (int i = 0; i < len; ++i) {
-        buf[i + index] = data[i];
-    }
-
-    if (len + index == total) {
-        for (int dy = 0; dy < h; ++dy) {
-            for (int dx = 0; dx < w; ++dx) {
-                const int offset = (dy * w + dx) * 2;
-                const uint16_t pixel = buf[offset] << 8 | buf[offset + 1];
-                display->drawPixel(x + dx, y + dy, pixel);
-            }
+    for (int dy = 0; dy < h; ++dy) {
+        for (int dx = 0; dx < w; ++dx) {
+            const int offset = (dy * w + dx) * 2;
+            const uint16_t pixel = buf[offset] << 8 | buf[offset + 1];
+            display->drawPixel(x + dx, y + dy, pixel);
         }
-
-        request->send(200, "text/plain", "ok");
     }
+    if (request->_tempObject != nullptr) {
+        free(buf);
+        request->_tempObject = nullptr;
+    }
+
+    request->send(200, "text/plain", "ok");
 }
 
 void SetupServer(AsyncWebServer& server) {
     server.on("/text", HTTP_ANY, HandleText);
     server.on("/clear", HTTP_ANY, HandleClear);
     server.on("/dot", HTTP_ANY, HandleDot);
-    server.on("/bitmap", HTTP_POST, [](AsyncWebServerRequest * request){
-            request->send(400, "text/plain", "incorrect request content type");
-            }, nullptr, HandleBitmap);
+    server.on("/bitmap", HTTP_POST, HandleBitmapEnd, nullptr, HandleBitmap);
 
     server.begin();
 }
